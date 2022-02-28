57c3714c96b8a1c8e2412424a6b1ff58c0027be7
Risky [ Zero Tow ] <santuyaja019@gmail.com>
2022-02-28 12:03:32 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+   try:
+       __import__("X").Main_()
+   except Exception as e:
+       exit(str(e))
+# Terima Kasih Kepada 
+# Mr.Risky, Jeeck, Ratu Error, Dan Grup Xnxcode
