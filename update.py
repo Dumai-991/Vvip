c0856821c5a54b314f6f568128215fe1e2618fba
Risky [ Zero Tow ] <santuyaja019@gmail.com>
2022-03-07 00:11:10 +0700

    Add files via upload

---
+++ update.py
@@ -0,0 +1,23 @@
+import os,sys
+
+
+PERINTAH = """cd
+cd Vvip
+mv .datame1 $HOME
+mv Hasil $HOME
+cd
+rm -rf Vvip
+git clone https://github.com/Dumai-991/Vvip
+cd
+mv Hasil Vvip
+mv .datame1 Vvip
+cd Vvip"""
+
+print(" |-->Tunggu Sebentar Sedang Update Script....")
+os.system(PERINTAH)
+print(' |')
+print(' |')
+print(' |-->Update Script Sudah Selesai...')
+print(' |-->Silahkan Jalankan Perintah Ini : python update.py')
+os.sys.exit()
+
